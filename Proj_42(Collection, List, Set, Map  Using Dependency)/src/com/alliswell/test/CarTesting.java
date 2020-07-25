795128c828382e3ed93c970df61ead7a9bf8ab1f
DELL <DELL@ADMIN>
2020-07-25 19:52:05 +0530

    AW-529 Updated

--- Proj_42(Collection, List, Set, Map  Using Dependency)/src/com/alliswell/test/CarTesting.java
+++ Proj_42(Collection, List, Set, Map  Using Dependency)/src/com/alliswell/test/CarTesting.java
@@ -8,7 +8,7 @@ import com.alliswell.beans.Car;
 public class CarTesting {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/alliswell/common/application-context.xml");
-		Car car = ctx.getBean("car1", Car.class);
+		Car car = (Car) ctx.getBean("car1");
 		System.out.println(car);
 	}
 }
